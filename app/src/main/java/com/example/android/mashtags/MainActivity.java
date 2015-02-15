3b16e630a2e09ae3038562b61889af6a5f179218
Ken Durkin <kdurkin6@fordham.edu>
2015-02-14 23:29:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/mashtags/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.android.mashtags;
+
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new RhineAsyncTask().execute();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private static final String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    private class RhineAsyncTask extends AsyncTask<Void, Void, String>
+    {
+        @Override
+        protected String doInBackground(Void... params) {
+            String mApiKey = apiKey.getKey();
+            Rhine rhine = new Rhine(mApiKey);
+            String result = rhine.run(rhine.extraction(rhine.image("http://i.imgur.com/jaVav95.jpg")));
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView textView = (TextView) findViewById(R.id.example_text);
+            textView.setText(result);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/mashtags/Rhine.java
@@ -0,0 +1,69 @@
+package com.example.android.mashtags;
+
+import java.util.List;
+import java.net.URLConnection;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.DataOutputStream;
+
+public class Rhine {
+    public Rhine(String apikey) {
+        this.url = "http://api.rhine.io/";
+        this.apikey = apikey;
+    }
+
+    public String run (String request) {
+        String k = request.substring(request.indexOf("\"") + 1, request.indexOf(":") - 1);
+        //System.out.println(k);
+        request = "{\"request\": {\"method\": " + request + ", \"key\": \"" + this.apikey + "\"}}";
+        //System.out.println(request);
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(this.url);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            DataOutputStream d = new DataOutputStream(conn.getOutputStream());
+            d.writeBytes(request);
+            d.flush();
+            d.close();
+
+            InputStream i = conn.getInputStream();
+            BufferedReader r = new BufferedReader(new InputStreamReader(i));
+            return r.readLine();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            if (conn != null) conn.disconnect();
+        }
+    }
+
+    private String apikey;
+    private String url;
+
+    public static final String entity (String e) { return "{\"entity\": \"" + e + "\"}"; }
+    public static final String article (String url) { return "{\"article\": \"fromurl\": \"" + url + "\"}"; }
+    public static final String image (String url) { return "{\"image\":{\"fromurl\": \"" + url + "\"}}"; }
+    public static final String text (String t) { return "{\"text\": \"" + t + "\"}"; }
+
+    public static final String distance (String x, String y) { return "{\"distance\": [" + x + "," + y + "]}"; }
+    public static final String extraction (String x) { return "{\"extraction\": " + x + "}"; }
+    public static final String equivalence (String x, String y) { return "{\"equivalance\": [" + x + "," + y + "]}"; }
+    public static final String subclass (String x, String y) { return "{\"subclass\": [" + x + "," + y + "]}"; }
+    public static final String clustering (List<String> x) { String b = "{\"clustering\": ["; for (String e : x) b += e + ","; if (x.size() > 1) b = b.substring(0, b.length() - 1); return b + "]}"; }
+
+    /*public static void main(String[] args) {
+        Rhine x = new Rhine("NEKEMBIPUXPJYZCZIMAXXZJVK");
+        System.out.println(x.run(extraction(image("http://i.imgur.com/jaVav95.jpg"))));
+        //System.out.println(x.run(subclass(entity("Mango"), entity("Tasty"))));
+    }*/
+}
